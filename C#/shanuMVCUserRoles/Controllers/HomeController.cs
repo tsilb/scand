e3799617475eb1c6dffb470079b7404c85a0f718
Juan Valenzuela <jvalenzuela1994@gmail.com>
2019-04-03 17:27:07 -0500

    Revert "Merge branch 'master' of https://github.com/tsilb/scand"
    This reverts commit 6ef068d547a6f716a7a47014b0e78ad727c08763, reversing
    changes made to 59a85d826637233edc0cea8279b7048f483781ca.

--- C#/shanuMVCUserRoles/Controllers/HomeController.cs
+++ C#/shanuMVCUserRoles/Controllers/HomeController.cs
@@ -3,36 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Data.SqlClient;
-using shanuMVCUserRoles.Models;
 
 namespace shanuMVCUserRoles.Controllers
 {
 	public class HomeController : Controller
 	{
 		public ActionResult Index()
-		{          
-            String connectionString = "Server=tcp:scand2.database.windows.net,1433;Initial Catalog=AttendanceDB;Persist Security Info=False;User ID=scandadmin;Password=Capstone1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-            String sql = "SELECT * FROM Logs";        
-
-            var model = new List<ScannedViewModel>();
-            using (SqlConnection conn = new SqlConnection(connectionString))
-            {
-                SqlCommand cmd = new SqlCommand(sql, conn);
-                conn.Open();
-                SqlDataReader rdr = cmd.ExecuteReader();
-                while (rdr.Read())
-                {
-                    var scanned = new ScannedViewModel();
-                    scanned.UserName = rdr["UserName"].ToString();
-                    scanned.Room = rdr["Room"].ToString();
-                    scanned.Time = rdr["Time"].ToString();
-
-                    model.Add(scanned);
-                }
-            }
-            return View(model);
-        }
+		{
+			return View();
+		}
 
 		public ActionResult About()
 		{
@@ -47,5 +26,5 @@ namespace shanuMVCUserRoles.Controllers
 
 			return View();
 		}
-    }
+	}
 }
