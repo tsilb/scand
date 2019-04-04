37d4a035e25c7d64c3afcb9d2a9f2db87139120c
tsilb <tsilb7@gmail.com>
2019-04-04 13:57:08 -0500

    Creation of new entries works.

--- C#/shanuMVCUserRoles/Controllers/HomeController.cs
+++ C#/shanuMVCUserRoles/Controllers/HomeController.cs
@@ -5,36 +5,96 @@ using System.Web;
 using System.Web.Mvc;
 using System.Data.SqlClient;
 using shanuMVCUserRoles.Models;
+using System.Data;
 
 namespace shanuMVCUserRoles.Controllers
 {
 	public class HomeController : Controller
 	{
-		public ActionResult Index()
-		{          
-            String connectionString = "Server=tcp:scand2.database.windows.net,1433;Initial Catalog=AttendanceDB;Persist Security Info=False;User ID=scandadmin;Password=Capstone1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-            String sql = "SELECT * FROM Logs";        
-
-            var model = new List<ScannedViewModel>();
-            using (SqlConnection conn = new SqlConnection(connectionString))
+        string connectionString = "Server=tcp:scand2.database.windows.net,1433;Initial Catalog=AttendanceDB;Persist Security Info=False;User ID=scandadmin;Password=Capstone1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        [HttpGet]
+        public ActionResult Index()
+        {
+            DataTable dtblScanned = new DataTable();
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
             {
-                SqlCommand cmd = new SqlCommand(sql, conn);
-                conn.Open();
-                SqlDataReader rdr = cmd.ExecuteReader();
-                while (rdr.Read())
-                {
-                    var scanned = new ScannedViewModel();
-                    scanned.UserName = rdr["UserName"].ToString();
-                    scanned.Room = rdr["Room"].ToString();
-                    scanned.Time = rdr["Time"].ToString();
-
-                    model.Add(scanned);
-                }
+                sqlCon.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT * FROM Logs ORDER BY ID DESC", sqlCon);
+                sqlDa.Fill(dtblScanned);
             }
-            return View(model);
+            return View(dtblScanned);
         }
 
-		public ActionResult About()
+        // GET: Home/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View(new ScannedViewModel());
+        }
+
+        // POST: Home/Create
+        [HttpPost]
+        public ActionResult Create(ScannedViewModel scannedViewModel)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                string ins = "INSERT INTO Logs VALUES(@UserName,@Room,@Time)";
+                SqlCommand sqlCmd = new SqlCommand(ins, sqlCon);
+                sqlCmd.Parameters.AddWithValue("@UserName", scannedViewModel.UserName);
+                sqlCmd.Parameters.AddWithValue("@Room", scannedViewModel.Room);
+                sqlCmd.Parameters.AddWithValue("@Time", scannedViewModel.Time);
+                sqlCmd.ExecuteNonQuery();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: Scanned/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Scanned/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Scanned/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Scanned/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult About()
 		{
 			ViewBag.Message = "Your application description page.";
 
